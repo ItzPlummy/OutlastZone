57551b8b3046b7f1c112cf01214369084cea8e60
Plummy <plummybeatsoff@gmail.com>
2026-08-31 18:59:33 +0200

    Add tool and player to block break event

--- src/main/java/com/plummy/outlastzone/listeners/enhancement/AppleEnhancementListener.java
+++ src/main/java/com/plummy/outlastzone/listeners/enhancement/AppleEnhancementListener.java
@@ -3,9 +3,9 @@ package com.plummy.outlastzone.listeners.enhancement;
 import com.plummy.outlastzone.core.games.Game;
 import com.plummy.outlastzone.core.games.GamePhase;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDropItemEvent;
@@ -16,7 +16,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
-import static com.plummy.outlastzone.OutlastZone.*;
+import static com.plummy.outlastzone.OutlastZone.getGameManager;
+import static com.plummy.outlastzone.OutlastZone.getSettings;
 
 public class AppleEnhancementListener implements Listener {
 
@@ -40,7 +41,7 @@ public class AppleEnhancementListener implements Listener {
             event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), drop);
         }
 
-        dropApples(event.getBlock().getState());
+        dropApples(event.getBlock().getState(), null, null);
 
         event.getBlock().setType(Material.AIR);
     }
@@ -60,15 +61,15 @@ public class AppleEnhancementListener implements Listener {
         if (event.getPlayer().getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.SILK_TOUCH) > 0) return;
 
         event.getItems().removeIf(item -> item.getItemStack().getType() == Material.APPLE);
-        dropApples(event.getBlockState());
+        dropApples(event.getBlockState(), event.getPlayer().getInventory().getItemInMainHand(), event.getPlayer());
     }
 
-    private void dropApples(BlockState block) {
+    private void dropApples(BlockState block, ItemStack tool, Entity entity) {
         int minModifier = getSettings().getAppleDropIncreaseMinModifier();
         int maxModifier = getSettings().getAppleDropIncreaseMaxModifier();
 
         for (int index = 0; index < ThreadLocalRandom.current().nextInt(minModifier, maxModifier + 1); index++) {
-            Collection<ItemStack> appleDrops = block.getDrops().stream().filter(itemStack -> itemStack.getType() == Material.APPLE).toList();
+            Collection<ItemStack> appleDrops = block.getDrops(tool, entity).stream().filter(itemStack -> itemStack.getType() == Material.APPLE).toList();
 
             for (ItemStack drop : appleDrops) {
                 block.getWorld().dropItemNaturally(block.getLocation(), drop);
